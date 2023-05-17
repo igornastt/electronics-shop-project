2c037074245366306706184dbd884684357e925e
igornast <grezle00@gmail.com>
2023-05-17 18:12:47 +0300

    Создание тестов для представления товара в магазине.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_items():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
+    assert isinstance(item1, Item)
+
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000
