65ee3167bc2dfd1065b28586f0d1ab9aa2d464df
igornast <grezle00@gmail.com>
2023-05-21 16:31:08 +0300

    Прописаны 3 метода с декораторами property, classmethod, staticmethod

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -14,9 +17,11 @@ class Item:
         :param quantity: Количество товара в магазине.
         """
 
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +37,42 @@ class Item:
         self.price *= self.pay_rate
 
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+
+        try:
+            self.__name = name
+            if len(self.name) <= 10:
+                return True
+        except Exception:
+            return "Длина наименования товара превышает 10 символов"
+
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        '''
+        Инициализирует экземпляры класса,
+        получает обЪекты из файла  csv
+        '''
+
+        cls.all.clear()
+        try:
+            with open('../src/items.csv', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cls(row['name'], row['price'], row['quantity'])
+        except FileNotFoundError:
+            print("Файл не найден")
+
+    @staticmethod
+    def string_to_number(line):
+        '''
+        Возвращает число из числа-строки
+        '''
+        line = float(line)
+        return int(line)
