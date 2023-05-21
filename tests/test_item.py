5e99a42946ba3c19d9eefa42582a9715f8bd93b8
igornast <grezle00@gmail.com>
2023-05-21 16:31:59 +0300

    Добавлены 2 теста

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,3 +10,18 @@ def test_items():
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
