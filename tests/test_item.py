dba29f66bd9b365f146c26042ab25169115148c2
igornast <grezle00@gmail.com>
2023-05-31 11:09:06 +0300

    Добавлены тесты к классу Phone

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
-import pytest
+from src.phone import Phone
 
 
 def test_items():
@@ -34,3 +34,26 @@ def test_repr():
 def test_str():
     item1 = Item("Смартфон", 10000, 20)
     assert str(item1) == 'Смартфон'
+
+
+def test_repr():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+
+def test_str():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
+    assert phone1.number_of_sim == 2
+
+
+def test_add():
+    item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+
+def test_number_of_sim():
+    phone1 = Phone("iPhone 14", 120_000, 5, 0)
+    assert phone1.number_of_sim == 0
