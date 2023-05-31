c33d2b426b3b4b8172af3274de8f11949f3fa190
igornast <grezle00@gmail.com>
2023-05-31 11:07:51 +0300

    Тесты к классу Phone

---
+++ tests/test_phone.py
@@ -0,0 +1,25 @@
+from src.item import Item
+from src.phone import Phone
+
+
+def test_repr():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
+
+
+def test_str():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert str(phone1) == 'iPhone 14'
+    assert phone1.number_of_sim == 2
+
+
+def test_add():
+    item1 = Item("Смартфон", 10000, 20)
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+
+def test_number_of_sim():
+    phone1 = Phone("iPhone 14", 120_000, 5, 0)
+    assert phone1.number_of_sim == 0
